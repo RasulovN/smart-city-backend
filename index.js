ae5b7f249c67198c9581ebb06ddf1297d1750577
unknown <nurbekrasulov71@gmail.com>
2025-12-03 11:44:04 +0500

    fix2

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ swaggerSetup(app);
 // Middlewares
 app.use(cors({
   origin:  "*",
-  credentials: true,
+  // credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
