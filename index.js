ff3e807e1b288bf35431c5f7f289530afa4af749
unknown <nurbekrasulov71@gmail.com>
2025-12-15 09:47:02 +0500

    fix2

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ swaggerSetup(app);
 
 app.use(cookieParser());
 app.use(cors({
-  origin: ["*", "http://localhost:5173", "http://localhost:3000", "https://admin.smart-city-qarshi.uz", "https://smart-city-qarshi.uz", "https://api.smart-city-qarshi.uz"],
+  origin: ["*", "http://localhost:5173", "http://localhost:3000", "https://admin.smart-city-qarshi.uz", "https://smart-city-qarshi.uz", "https://api.smart-city-qarshi.uz","https://contol.smart-city-qarshi.uz", "https://contol.smart-city-qarshi.uz/uz/login", ],
   methods: ["GET","POST","PUT","DELETE"],
   allowedHeaders: ["Content-Type","Authorization"],
   credentials: true,
